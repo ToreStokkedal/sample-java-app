6584d8b87e34da27355186a06d9d0e46331b1416
Tore Stokkedal <tore.stokkedal@gmail.com>
2021-08-10 23:00:18 +0200

    first commit

---
+++ src/test/java/no/stokkedal/NativeSampleAppIT.java
@@ -0,0 +1,9 @@
+package no.stokkedal;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeSampleAppIT extends SampleAppTest {
+
+    // Execute the same tests but in native mode.
+}
